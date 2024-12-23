e125c94db404dfbe8502fc16023ae4b1a1437df1
Spax <83354374+SpiritAxolotl@users.noreply.github.com>
2024-12-23 01:26:20 -0700

    initial commit!

---
+++ Bonne_Soiree.lua
@@ -0,0 +1,77 @@
+SMODS.Atlas({
+  key = "modicon",
+  path = "icon.png",
+  px = 48,
+  py = 48
+})
+
+SMODS.Sound({
+  vol = 0.6,
+  pitch = 0.7,
+  key = "music1",
+  path = "music1.ogg",
+  replace = "music1",
+  sync = {
+    soiree_music1 = true,
+    soiree_music2 = true,
+    soiree_music3 = true,
+    soiree_music4 = true,
+    soiree_music5 = true
+  }
+})
+SMODS.Sound({
+  vol = 0.6,
+  pitch = 0.7,
+  key = "music2",
+  path = "music2.ogg",
+  replace = "music2",
+  sync = {
+    soiree_music1 = true,
+    soiree_music2 = true,
+    soiree_music3 = true,
+    soiree_music4 = true,
+    soiree_music5 = true
+  }
+})
+SMODS.Sound({
+  vol = 0.6,
+  pitch = 0.7,
+  key = "music3",
+  path = "music3.ogg",
+  replace = "music3",
+  sync = {
+    soiree_music1 = true,
+    soiree_music2 = true,
+    soiree_music3 = true,
+    soiree_music4 = true,
+    soiree_music5 = true
+  }
+})
+SMODS.Sound({
+  vol = 0.6,
+  pitch = 0.7,
+  key = "music4",
+  path = "music4.ogg",
+  replace = "music4",
+  sync = {
+    soiree_music1 = true,
+    soiree_music2 = true,
+    soiree_music3 = true,
+    soiree_music4 = true,
+    soiree_music5 = true
+  }
+})
+SMODS.Sound({
+  vol = 0.6,
+  pitch = 0.7,
+  key = "music5",
+  path = "music5.ogg",
+  replace = "music5",
+  sync = {
+    soiree_music1 = true,
+    soiree_music2 = true,
+    soiree_music3 = true,
+    soiree_music4 = true,
+    soiree_music5 = true
+  }
+})
